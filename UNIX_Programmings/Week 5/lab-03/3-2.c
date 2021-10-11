d9fe5f77b6341c32615cfa79535be2de44c8a897
arad4228 <arad4228@gmail.com>
2021-10-11 15:43:50 +0900

    5주차과제코드 및 과제 수정

--- UNIX_Programmings/Week 5/lab-03/3-2.c
+++ UNIX_Programmings/Week 5/lab-03/3-2.c
@@ -1,6 +1,8 @@
+#include <sys/stat.h>
 #include <fcntl.h>
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(void)
--- UNIX_Programmings/Week 5/lab-03/3-3.c
+++ UNIX_Programmings/Week 5/lab-03/3-3.c
@@ -1,3 +1,5 @@
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
