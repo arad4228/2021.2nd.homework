fb2b00185d26b46f95eb8b2195e6769a0d226e28
arad4228 <arad4228@gmail.com>
2021-11-03 21:10:49 +0900

    과제 제네릭스를 통한 구현

---
+++ object_oriented/homework 6/MP06(Generics)/src/DAO.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public interface DAO<D,K> {
+
+    public void insert(D data);
+    public List<D> findAll();
+    public D findByKey(K key);
+    public void update(D data);
+    public void deleteByKey(K key);
+    public void delete(D data);
+}
